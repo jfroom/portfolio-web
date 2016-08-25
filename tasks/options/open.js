2cd49a378150c0f145a0fde871471001c13e44b2
jfroom <jeff@jfroom.com>
2016-08-25 16:42:52 -0700

    Grunt open uses chrome canary

--- tasks/options/open.js
+++ tasks/options/open.js
@@ -2,6 +2,6 @@ var nconf = require("nconf");
 module.exports = {
   server: {
     path: 'http://' + nconf.get("SERVER_HOSTNAME") + ':' + nconf.get("SERVER_PORT") +'/',
-    app: 'Firefox' //'Google Chrome Canary'
+    app: 'Google Chrome Canary'
   }
 };
