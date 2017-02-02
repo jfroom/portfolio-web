605dda72c7068c3d1dd3a8b50a4a703e599fd0a6
jfroom <jeff@jfroom.com>
2017-02-02 15:44:44 -0800

    Change S3 task to include dest path

--- tasks/options/s3.js
+++ tasks/options/s3.js
@@ -5,12 +5,15 @@ module.exports = {
     bucket: "<%= process.env['S3_PORTFOLIO_BUCKET'] %>",
     region: "<%= process.env['S3_PORTFOLIO_REGION'] %>",
     access: 'public-read',
+    //cache: false, // set to false to force overwrite uploads
+    //dryRun: true // set to true to see logging of what will happen, but not actually upload
+
   },
 
   dist: {
     cwd: 'dist',
-    src: '**' //,
-    //dest: process.env.S3_PORTFOLIO_PATH
+    src: '**',
+    dest: "<%= process.env['S3_PORTFOLIO_PATH'] %>",
   }
 
 };
