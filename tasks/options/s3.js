452786c59b36ba13f0dea06c88e587847a8273f7
jfroom <jeff@jfroom.com>
2017-02-03 01:42:18 -0800

    S3 task allows alternate access rights for index.html

--- tasks/options/s3.js
+++ tasks/options/s3.js
@@ -4,16 +4,27 @@ module.exports = {
     secretAccessKey: "<%= process.env['S3_PORTFOLIO_SECRET'] %>",
     bucket: "<%= process.env['S3_PORTFOLIO_BUCKET'] %>",
     region: "<%= process.env['S3_PORTFOLIO_REGION'] %>",
-    access: 'public-read',
-    //cache: false, // set to false to force overwrite uploads
+    access: "<%= process.env['S3_PORTFOLIO_ACCESS'] %>",
+    gzip: true,
+    cache: false, 
+    overwrite: true,
     //dryRun: true // set to true to see logging of what will happen, but not actually upload
-
   },
 
   dist: {
     cwd: 'dist',
     src: '**',
+    dest: "<%= process.env['S3_PORTFOLIO_PATH'] %>"
+  },
+
+  index_page: {
+    cwd: 'dist',
+    src: 'index.html',
     dest: "<%= process.env['S3_PORTFOLIO_PATH'] %>",
+    options: {
+      // Personal app needs this to be private - so can be consumed by private rails app
+      access: "<%= process.env['S3_PORTFOLIO_INDEX_ACCESS'] %>"
+    }
   }
 
 };
--- tasks/upload_task.js
+++ tasks/upload_task.js
@@ -1,6 +1,7 @@
 module.exports = function(grunt) {
   grunt.registerTask('upload', [
   	'production',
-    's3'
+    's3:dist',
+    's3:index_page'
   ]);
 };
